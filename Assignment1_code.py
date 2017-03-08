56d462996e0f0d9438172fe6c350327ef8dd780a
Max Hollmann <maxhollmann@gmail.com>
2017-03-08 13:58:44 +0100

    half a radius

--- Assignment1_code.py
+++ Assignment1_code.py
@@ -9,9 +9,12 @@ import os
 import numpy as np
 
 # Set working directory to location of this file
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
+try:
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+except:
+    pass
 
 # Our data
 test_in = np.loadtxt("data/test_in.csv", delimiter = ',')
@@ -21,7 +24,17 @@ train_out = np.loadtxt("data/train_out.csv", delimiter = ',')
 
 # Proceed with train_in and train_out, use test_in and test_out to check prediction later
 
+centers = np.array([None] * 10)
+radiuses = np.array([None] * 10)
+
 for d in range(0, 10):
     train_d = train_in[train_out == d, ]
     c_d = np.mean(train_d, 0)
-    
+
+    diff = [
+        [px - ]
+    ]
+    r_d = [img - c_d for img in train_d]
+
+    centers[d] = c_d
+    radiuses[d] = r_d
